bd6da48dbf9ca77368250d4fc60937c9cefbed4a
Armaan Ballagan <a.ballagan@surreyschools.ca>
2022-12-14 14:53:54 -0800

    jason fjeowajfioj

--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -19,13 +19,17 @@ import javax.swing.JPanel;
 public class Sudoku extends JFrame {
     //TicTacToe "is a" JFrame
     Button[][] buttonGrid;
+    static int[][] smallSqaure;
+    static int squareRows = 3;
+    static int squareCols = 3;
     static int Rows = 9;
     static int Cols = 9;
     boolean gameOver;
     JLabel message;
+    JLabel squareMessage;
     static boolean t = true;
     static boolean f = false;
-    int[][] allValues = {{7,5,3,2,8,4,1,6,9},
+    static int[][] allValues = {{7,5,3,2,8,4,1,6,9},
                         {2,1,8,3,9,6,7,5,4},
                         {4,9,6,1,5,7,8,2,3},
                         {6,4,9,8,3,1,2,7,5},
@@ -33,36 +37,68 @@ public class Sudoku extends JFrame {
                         {5,3,1,6,7,2,9,4,8},
                         {9,6,2,4,1,5,3,8,7},
                         {1,8,5,7,6,3,4,9,2},
-                        {3,7,4,9,2,8,5,1,6]};
-    
-    boolean[][] givenValues = {{t,t,f,t,f,f,t,f,t}};
+                        {3,7,4,9,2,8,5,1,6}};
             
-    boolean[][] givenValues =  {[t,t,f,t,f,4,1,6,9],
-                                [2,t,8,3,t,t,7,5,t],
-                                [t,9,6,1,t,7,8,2,t],
-                                [6,4,9,8,t,1,2,7,t],
-                                [8,2,t,5,t,9,6,t,1],
-                                [t,3,t,6,7,t,t,4,8],
-                                [t,6,2,4,1,5,t,t,t],
-                                [1,8,5,7,t,3,4,9,2],
-                                [3,7,t,9,2,t,t,t,6]};
- /*   public Sudoku() {
-        
-        //JFrame frame = new JFrame("Tic Tac Toe");
+    static boolean[][] givenValues =  {{t,t,f,t,f,f,f,f,f},
+                                {f,t,f,f,t,t,f,f,t},
+                                {t,f,f,f,t,f,f,f,t},
+                                {f,f,f,f,t,f,f,f,t},
+                                {f,f,t,f,t,f,f,t,f},
+                                {t,f,t,f,f,t,t,f,f},
+                                {t,f,f,f,f,f,t,t,t},
+                                {f,f,f,f,t,f,f,f,f},
+                                {f,f,t,f,f,t,t,t,f}};
+    public Sudoku() {
+        //current values grid = given values grid?
+    
+        //JFrame frame = new JFrame("Sudoku");
         super("Sudoku");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         
         JPanel messagePanel = new JPanel();
-        message = new JLabel("X's turn!");
+        message = new JLabel("Sudoku");
         message.setFont(new Font(Font.SERIF, Font.ITALIC, 36));
-        
         messagePanel.add(message, BorderLayout.NORTH);
         this.add(messagePanel);
         
+        /*
         GridLayout lm = new GridLayout(Rows,Cols);
         JPanel buttonPanel = new JPanel(lm);
         buttonGrid = new Button[Rows][Cols];
+        */
+        GridLayout sudokuLayoutManager = new GridLayout(squareRows,squareCols);
+        JPanel sudokuPanel = new JPanel(sudokuLayoutManager);
+        
+        
+        GridLayout squareLayoutManager = new GridLayout(squareRows,squareCols);
+        
+        for(int r=0; r<squareRows; r++) {
+            for(int c=0; c<squareCols; c++) {
+             
+            JPanel squarePanel = new JPanel(squareLayoutManager);
+            
+            squareMessage = new JLabel("hi");
+            squareMessage.setFont(new Font(Font.SERIF, Font.ITALIC, 36));
+            squarePanel.add(squareMessage);
+            for(int i=0; i<squareRows; i++) {
+                for(int j=0; j<squareCols; j++) {
+                    
+                    
+                }
+            }     
+            
+            sudokuPanel.add(squarePanel);
+            
+        }
+            
+            
+        }
+        
+        /*buttonGrid = new Button[Rows][Cols];
+        
+        for 
+        
         
         for (int i = 0; i< Rows; i++){
             for(int j=0; j< Cols; j++){
@@ -71,14 +107,13 @@ public class Sudoku extends JFrame {
                 b.addActionListener(tch);
                 buttonGrid[i][j] = b;
                 buttonPanel.add(b);
-                
             }
         }
-        this.add(buttonPanel, BorderLayout.SOUTH );
-        this.pack();
+        this.add(buttonPanel, BorderLayout.SOUTH ); */
+        this.add(sudokuPanel, BorderLayout.SOUTH);
         gameOver = false;
     }
-}
+
 /*    
     public void Move(Button tile){
         if (!tile.claimed && !gameOver) {
@@ -160,7 +195,16 @@ public class Sudoku extends JFrame {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        for (int r=0; r < Rows; r++) {
+            for (int c=0; c< Cols; c++) {
+                if (givenValues[r][c] == true) {
+                        System.out.print(allValues[r][c]);
+            } else{
+                    System.out.print(" ");
+                    }
+            }
+            System.out.println();
+        }
         Sudoku game = new Sudoku();
     }
     
